4ced2fd0c7988e5873e2f88a55f394da75a765ca
AlejandroCrespoJ <alejandro.crespo@udla.edu.ec>
2026-05-08 15:13:16 -0500

    imprimo la pantalla de menu para elegir as opciones y a a lves ecaneo estas

--- calculadora.c
+++ calculadora.c
@@ -3,3 +3,25 @@
 #include"suma.h"
 #include"resta.h"
 #include"multiplicacion.h"
+
+int main(){
+
+     float n1, n2;
+    int opcion;
+
+    printf("CALCULADORA\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicacion\n");
+    printf("4. Division\n");
+
+    printf("Elija una opcion: ");
+    scanf("%d", &opcion);
+
+    printf("Ingrese el primer numero: ");
+    scanf("%f", &n1);
+
+    printf("Ingrese el segundo numero: ");
+    scanf("%f", &n2);
+
+}
